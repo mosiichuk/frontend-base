cf21b2883d415c96d9d93d0b07ffd965ccdc3a34
Yevhen Mosiichuk <zhenya9801@gmail.com>
2021-02-17 15:29:29 +0200

    Uncomment images loaders

--- config/webpack.loaders.js
+++ config/webpack.loaders.js
@@ -78,49 +78,49 @@ const html = {
 //         },
 //     ],
 // };
-//
-// const imageLoader = {
-//     loader: 'image-webpack-loader',
-//     options: {
-//         disable: config.env === 'development',
-//         gifsicle: {
-//             interlaced: false,
-//         },
-//         optipng: {
-//             optimizationLevel: 7,
-//         },
-//         pngquant: [
-//             '65-90',
-//             4,
-//         ],
-//         mozjpeg: {
-//             progressive: true,
-//         }
-//     },
-// };
-//
-// const images = {
-//     test: /\.(gif|png|jpe?g|svg)$/,
-//     include: [
-//         path.resolve(__dirname, '../src/img/')
-//     ],
-//     use: [
-//         'file-loader?name=img/[name].[hash].[ext]&esModule=false',
-//         imageLoader
-//     ]
-// };
 
-// const icons = {
-//     test: /\.(gif|png|jpe?g|svg)$/,
-//     include: [
-//         path.resolve(__dirname, '../src/icons/')
-//     ],
-//     use: [
-//         'file-loader?name=icons/[name].[hash].[ext]&esModule=false',
-//         imageLoader
-//     ]
-// };
+const imageLoader = {
+    loader: 'image-webpack-loader',
+    options: {
+        disable: config.env === 'development',
+        gifsicle: {
+            interlaced: false,
+        },
+        optipng: {
+            optimizationLevel: 7,
+        },
+        pngquant: [
+            '65-90',
+            4,
+        ],
+        mozjpeg: {
+            progressive: true,
+        }
+    },
+};
 
+const images = {
+    test: /\.(gif|png|jpe?g|svg)$/,
+    include: [
+        path.resolve(__dirname, '../src/img/')
+    ],
+    use: [
+        'file-loader?name=img/[name].[hash].[ext]&esModule=false',
+        imageLoader
+    ]
+};
+
+const icons = {
+    test: /\.(gif|png|jpe?g|svg)$/,
+    include: [
+        path.resolve(__dirname, '../src/icons/')
+    ],
+    use: [
+        'file-loader?name=icons/[name].[hash].[ext]&esModule=false',
+        imageLoader
+    ]
+};
+//
 // const fonts = {
 //     test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
 //     exclude: [
@@ -141,7 +141,7 @@ module.exports = [
     // js,
     // css,
     // sass,
-    // images,
-    // icons,
+    images,
+    icons,
     // fonts,
 ];
